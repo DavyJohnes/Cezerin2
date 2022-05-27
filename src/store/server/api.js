eeb6d7174c457617fd6d600f14163c5860937d1e
Ivan Zuev <dajohnes@gmail.com>
2022-05-27 12:25:32 +0300

    fix: do not provide auth token in dev mode

--- src/store/server/api.js
+++ src/store/server/api.js
@@ -3,7 +3,7 @@ import CezerinClient from "cezerin2-client"
 import serverSettings from "./settings"
 
 const tokenPayload = { email: "store", scopes: ["admin"] }
-const storeAccessToken = jwt.sign(tokenPayload, serverSettings.jwtSecretKey)
+const storeAccessToken = serverSettings.developerMode ? "" : jwt.sign(tokenPayload, serverSettings.jwtSecretKey)
 
 const api = new CezerinClient({
   apiBaseUrl: serverSettings.apiBaseUrl,
